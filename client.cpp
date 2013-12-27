466c7d0d85e53012e9d43607e636a37b2dea1536
Ray Fung <posixfung@163.com>
2013-12-27 17:37:56 +0800

    exit when ip_connect() failed

--- client.cpp
+++ client.cpp
@@ -166,6 +166,8 @@ int main( int argc, char **argv )
     }
 
     sock = ip_connect( SOCK_STREAM, IPPROTO_TCP, host, port );
+    if(sock == -1)
+        exit(1);
 
     /* Associate SSL connection with server socket */
     ssl = SSL_new( ctx );
